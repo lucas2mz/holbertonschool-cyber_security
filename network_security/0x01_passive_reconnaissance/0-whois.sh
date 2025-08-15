38c50d1ddf63e30d8b811f380f8d5d98c58c2a04
lucas2mz <lucasandrada2606@gmail.com>
2025-08-14 21:58:20 -0300

    Task 0

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,21 +1,2 @@
 #!/bin/bash
-sudo whois "$1" | awk '
-BEGIN {
-    print "Registrant Organization,Registrant State/Province,Registrant Country,Registrant Email,Admin Organization,Admin State/Province,Admin Country,Admin Email,Tech Organization,Tech State/Province,Tech Country,Tech Email"
-}
-/Registrant Organization:/ {rorg=$2; for(i=3;i<=NF;i++){rorg=rorg" "$i}}
-/Registrant State\/Province:/ {rstate=$2; for(i=3;i<=NF;i++){rstate=rstate" "$i}}
-/Registrant Country:/ {rcountry=$2}
-/Registrant Email:/ {remail=$0; sub(/Registrant Email:/,"",remail)}
-/Admin Organization:/ {aorg=$2; for(i=3;i<=NF;i++){aorg=aorg" "$i}}
-/Admin State\/Province:/ {astate=$2; for(i=3;i<=NF;i++){astate=astate" "$i}}
-/Admin Country:/ {acountry=$2}
-/Admin Email:/ {aemail=$0; sub(/Admin Email:/,"",aemail)}
-/Tech Organization:/ {torg=$2; for(i=3;i<=NF;i++){torg=torg" "$i}}
-/Tech State\/Province:/ {tstate=$2; for(i=3;i<=NF;i++){tstate=tstate" "$i}}
-/Tech Country:/ {tcountry=$2}
-/Tech Email:/ {temail=$0; sub(/Tech Email:/,"",temail)}
-END {
-    print rorg","rstate","rcountry","remail","aorg","astate","acountry","aemail","torg","tstate","tcountry","temail
-}
-' > $1.csv
+sudo whois "$1" | awk 'BEGIN{print "Registrant Organization,Registrant State/Province,Registrant Country,Registrant Email,Admin Organization,Admin State/Province,Admin Country,Admin Email,Tech Organization,Tech State/Province,Tech Country,Tech Email"} /Registrant/ || /Admin/ || /Tech/{gsub(/: /,","); printf "%s,", $0} END{print ""}' > $1.csv
