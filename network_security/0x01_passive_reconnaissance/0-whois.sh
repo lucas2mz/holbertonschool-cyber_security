1d6057c051d6d6cc0ebde9d0cbaa0297acad9b47
lucas2mz <lucasandrada2606@gmail.com>
2025-08-17 20:23:59 -0300

    Task 0 please

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois $1 | awk '/Registrant/ || /Admin/ || /Tech/ {gsub(/^ +|: /,","); print}' > $1.csv
+whois "$1" | awk '/^(Registrant|Admin|Tech)/ {gsub(/^ +/,""); sub(": ",","); print}' > "$1.csv"
