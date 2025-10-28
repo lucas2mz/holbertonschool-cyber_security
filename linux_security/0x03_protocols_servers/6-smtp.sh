3aba844f9135772fc36cb06a7ff643af1455773b
lucas2mz <lucasandrada2606@gmail.com>
2025-10-28 10:54:15 -0300

    Task 6

--- linux_security/0x03_protocols_servers/6-smtp.sh
+++ linux_security/0x03_protocols_servers/6-smtp.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grep -E "^smtpd_tls_security_level\s*=" /etc/postfix/main.cf | sed 's/^smtpd_tls_security_level\s*=\s*//'
+grep -E "^smtpd_tls_security_level\s*=" /etc/postfix/main.cf || echo "STARTTLS not configured"
