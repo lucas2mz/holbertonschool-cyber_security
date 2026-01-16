0fa438546a291f27f26b33ee43a96d5b4aadfca3
lucas2mz <lucasandrada2606@gmail.com>
2026-01-16 12:23:57 -0300

    Task 5

---
+++ scripting_cyber/0x00-ruby_scripting/5-cipher.rb
@@ -0,0 +1,32 @@
+class CaesarCiphe
+
+    def initialize(shift)
+        @shift = shift
+    end
+
+    def encrypt(message)
+        cipher(message, @shift)
+    end
+
+    def decrypt(message)
+        cipher(message, -@shift)
+    end
+
+    private
+
+    def cipher(message, shift)
+        result = ""
+
+        message.each_char do |char|
+            if char >= 'a' && char <= 'z'
+                result << (((char.ord - 'a'.ord + shift) % 26) + 'a'.ord).chr
+            elsif char >= 'A' && char <= 'Z'
+                result << (((char.ord - 'A'.ord + shift) % 26) + 'Z'.ord).chr
+            else
+                result << char
+            end
+        end
+
+        result
+    end
+end
