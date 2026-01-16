cb69f5521bf15fe522d9915ffe3e420f0260e9d7
lucas2mz <lucasandrada2606@gmail.com>
2026-01-16 12:16:34 -0300

    Task 4

--- scripting_cyber/0x00-ruby_scripting/4-write_file.rb
+++ scripting_cyber/0x00-ruby_scripting/4-write_file.rb
@@ -1,14 +1,14 @@
 require 'json'
 def merge_json_files(file1_path, file2_path)
 
-  file_1 = File.read(file1_path)
-  file_2 = File.read(file2_path)
+    file1 = File.read(file1_path)
+    file2 = File.read(file2_path)
 
-  content1 = JSON.parse(file_1)
-  content2 = JSON.parse(file_2)
+    content1 = JSON.parse(content1)
+    content2 = JSON.parse(content2)
 
-  merged_content = content1 + content2
+    merged_content = data2 + data1
 
-  File.write(file2_path, JSON.pretty_generate(merged_content))
-  puts "Merged JSON written to #{file2_path}"
+    File.write(file2_path, JSON.pretty_generate(merged_content))
+    puts "Merged JSON written to #{file2_path}"
 end
