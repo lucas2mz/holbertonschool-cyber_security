de792ac356dae9a97f059f48caab69aa07393fb0
lucas2mz <lucasandrada2606@gmail.com>
2026-01-16 12:13:10 -0300

    Task 4

---
+++ scripting_cyber/0x00-ruby_scripting/4-write_file.rb
@@ -0,0 +1,14 @@
+require 'json'
+
+def merge_json_files(file1_path, file2_path)
+  file_1 = File.read(file1_path)
+  file_2 = File.read(file2_path)
+
+  content1 = JSON.parse(file_1)
+  content2 = JSON.parse(file_2)
+
+  merged_content = content1 + content2
+
+  File.write(file2_path, JSON.pretty_generate(merged_content))
+  puts "Files merge into #{file2_path}"
+end
